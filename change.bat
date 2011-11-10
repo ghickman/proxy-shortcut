39062c5ad7a6735f10fd2a3abfc7b19f62ef147d
George Hickman <george@ghickman.co.uk>
2011-11-10 23:01:59 +0000

    put the registry query in a string

--- change.bat
+++ change.bat
@@ -1,6 +1,10 @@
+SET is_proxy_enabled=REG QUERY HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings /v ProxyEnable
+echo is_proxy_enabled
 REM Is the proxy on? Turn it off.
 regedit.exe /s "c:\proxy-off.reg"
 
 REM Is the proxy off? Turn it on.
-REG QUERY HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings /v ProxyEnable
-regedit.exe /s "c:\proxy-on.reg"
+if is_proxy_enabled (
+    regedit.exe /s "c:\proxy-on.reg"
+)
+
